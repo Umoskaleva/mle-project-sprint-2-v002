35ef3712406d627fa6bdd801338aff99c3e4bfe9
Uliana Bychkova <mle-user@epdhvutqtbjetketij4s.auto.internal>
2026-09-07 14:12:30 +0000

    создание csv для модели

---
+++ src/clean_users_churn_for_model.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import yaml
+
+
+def create_connection():
+    load_dotenv()
+    host = os.getenv("DB_DESTINATION_HOST")
+    port = os.getenv("DB_DESTINATION_PORT")
+    db_name = os.getenv("DB_DESTINATION_NAME")
+    username = os.getenv("DB_DESTINATION_USER")
+    password = os.getenv("DB_DESTINATION_PASSWORD")
+    if not all([host, port, db_name, username, password]):
+        raise ValueError("Не все переменные окружения для подключения к БД заданы")
+    dsn = (
+        f"postgresql+psycopg2://{username}:{password}"
+        f"@{host}:{port}/{db_name}?sslmode=require"
+    )
+    return create_engine(dsn)
+
+
+def main():
+    with open("params.yaml", "r", encoding="utf-8") as f:
+        params = yaml.safe_load(f)
+    table_name = params["db"]["table_name"]
+    engine = create_connection()
+    query = f"SELECT * FROM {table_name}"
+    data = pd.read_sql(query, engine)
+    Path("data/processed").mkdir(parents=True, exist_ok=True)
+    data.to_csv("data/processed/clean_users_churn_for_model.csv", index=False)
+if __name__ == "__main__":
+    main()
---
+++ src/run_churn_experiment_practic_v2.py
@@ -0,0 +1,40 @@
+import os
+from datetime import datetime
+
+import mlflow
+import pandas as pd
+import boto3
+import joblib
+import json
+import numpy as np
+from sklearn.metrics import confusion_matrix, roc_auc_score, precision_score, recall_score, f1_score, log_loss
+
+
+
+# Каждый запуск получит новое имя эксперимента
+EXPERIMENT_NAME = f"churn_uliana_v3_1"
+RUN_NAME = "log_model"
+
+# Настройка переменных окружения для Yandex Cloud S3
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "https://storage.yandexcloud.net"  # endpoint бакета от YandexCloud
+os.environ["AWS_ACCESS_KEY_ID"] = os.getenv("AWS_ACCESS_KEY_ID")  # получаем id ключа бакета из .env
+os.environ["AWS_SECRET_ACCESS_KEY"] = os.getenv("AWS_SECRET_ACCESS_KEY")  # получаем ключ бакета из .env
+
+# Создаем клиент S3
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    endpoint_url=os.environ["MLFLOW_S3_ENDPOINT_URL"]
+)
+
+# скачиваем модель в локальный файл
+s3.download_file(
+    os.getenv("AWS_BUCKET_NAME"),
+    "models/fitted_model.pkl",
+    "fitted_model.pkl"
+)
+
+# загружаем в переменную model
+model = joblib.load("fitted_model.pkl")
+
